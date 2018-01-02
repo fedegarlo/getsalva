0b9616d2db15d7537271027d10590825976461f5
Federico <fedegarcia@icloud.com>
2018-01-02 17:20:39 +0100

    Second commit

---
+++ server.js
@@ -0,0 +1,25 @@
+#!/bin/env node
+
+var ipaddress = process.env.HOST || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+    port      = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    express = require('express'),
+    app      = express(),
+    path = require('path'),
+    http = require('http'),
+    server = http.createServer(app);
+
+app.configure(function() {
+    var cacheTime = 0;
+    app.use(express.cookieParser());
+    app.use(express.bodyParser()); 
+    app.use(express.static(path.join(__dirname, 'public'), { maxAge: cacheTime }));
+});
+
+
+app.get('/', function(request, response) {
+    response.render('index.html');
+});
+
+server.listen(port, ipaddress);
+console.log('Listening  to  port ' + port);
+
