c3599ef8d75a00abf04a9df80b01e562031d8af2
Federico <fedegarcia@icloud.com>
2018-04-23 09:08:16 +0200

    EULA

--- public/main.bundle.js
+++ public/main.bundle.js
@@ -31,6 +31,7 @@ webpackEmptyAsyncContext.id = "../../../../../src/$$_lazy_route_resource lazy re
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_6__hero_detail_hero_detail_component__ = __webpack_require__("../../../../../src/app/hero-detail/hero-detail.component.ts");
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_7__fastpass_fastpass_component__ = __webpack_require__("../../../../../src/app/fastpass/fastpass.component.ts");
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_8__get_app_get_app_component__ = __webpack_require__("../../../../../src/app/get-app/get-app.component.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_9__eula_eula_component__ = __webpack_require__("../../../../../src/app/eula/eula.component.ts");
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -46,10 +47,12 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 
 
 
+
 var routes = [
     { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
     { path: 'help', component: __WEBPACK_IMPORTED_MODULE_3__help_help_component__["a" /* HelpComponent */] },
     { path: 'conditions', component: __WEBPACK_IMPORTED_MODULE_4__conditions_conditions_component__["a" /* ConditionsComponent */] },
+    { path: 'eula', component: __WEBPACK_IMPORTED_MODULE_9__eula_eula_component__["a" /* EulaComponent */] },
     { path: 'fastpass', component: __WEBPACK_IMPORTED_MODULE_7__fastpass_fastpass_component__["a" /* FastpassComponent */] },
     { path: 'privacy', component: __WEBPACK_IMPORTED_MODULE_5__privacy_privacy_component__["a" /* PrivacyComponent */] },
     { path: 'product/:id', component: __WEBPACK_IMPORTED_MODULE_6__hero_detail_hero_detail_component__["a" /* HeroDetailComponent */] },
@@ -141,14 +144,15 @@ var AppComponent = (function () {
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__dashboard_dashboard_component__ = __webpack_require__("../../../../../src/app/dashboard/dashboard.component.ts");
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_5__help_help_component__ = __webpack_require__("../../../../../src/app/help/help.component.ts");
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_6__conditions_conditions_component__ = __webpack_require__("../../../../../src/app/conditions/conditions.component.ts");
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_7__fastpass_fastpass_component__ = __webpack_require__("../../../../../src/app/fastpass/fastpass.component.ts");
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_8__privacy_privacy_component__ = __webpack_require__("../../../../../src/app/privacy/privacy.component.ts");
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_9__hero_detail_hero_detail_component__ = __webpack_require__("../../../../../src/app/hero-detail/hero-detail.component.ts");
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_10__get_app_get_app_component__ = __webpack_require__("../../../../../src/app/get-app/get-app.component.ts");
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_11__hero_service__ = __webpack_require__("../../../../../src/app/hero.service.ts");
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_12__message_service__ = __webpack_require__("../../../../../src/app/message.service.ts");
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_13__angular_common_http__ = __webpack_require__("../../../common/esm5/http.js");
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_14__environments_environment__ = __webpack_require__("../../../../../src/environments/environment.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_7__eula_eula_component__ = __webpack_require__("../../../../../src/app/eula/eula.component.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_8__fastpass_fastpass_component__ = __webpack_require__("../../../../../src/app/fastpass/fastpass.component.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_9__privacy_privacy_component__ = __webpack_require__("../../../../../src/app/privacy/privacy.component.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_10__hero_detail_hero_detail_component__ = __webpack_require__("../../../../../src/app/hero-detail/hero-detail.component.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_11__get_app_get_app_component__ = __webpack_require__("../../../../../src/app/get-app/get-app.component.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_12__hero_service__ = __webpack_require__("../../../../../src/app/hero.service.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_13__message_service__ = __webpack_require__("../../../../../src/app/message.service.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_14__angular_common_http__ = __webpack_require__("../../../common/esm5/http.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_15__environments_environment__ = __webpack_require__("../../../../../src/environments/environment.ts");
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -171,7 +175,8 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 
 
 
-if (__WEBPACK_IMPORTED_MODULE_14__environments_environment__["a" /* environment */].production) {
+
+if (__WEBPACK_IMPORTED_MODULE_15__environments_environment__["a" /* environment */].production) {
     Object(__WEBPACK_IMPORTED_MODULE_1__angular_core__["_12" /* enableProdMode */])();
 }
 var AppModule = (function () {
@@ -184,17 +189,18 @@ var AppModule = (function () {
                 __WEBPACK_IMPORTED_MODULE_4__dashboard_dashboard_component__["a" /* DashboardComponent */],
                 __WEBPACK_IMPORTED_MODULE_5__help_help_component__["a" /* HelpComponent */],
                 __WEBPACK_IMPORTED_MODULE_6__conditions_conditions_component__["a" /* ConditionsComponent */],
-                __WEBPACK_IMPORTED_MODULE_8__privacy_privacy_component__["a" /* PrivacyComponent */],
-                __WEBPACK_IMPORTED_MODULE_7__fastpass_fastpass_component__["a" /* FastpassComponent */],
-                __WEBPACK_IMPORTED_MODULE_9__hero_detail_hero_detail_component__["a" /* HeroDetailComponent */],
-                __WEBPACK_IMPORTED_MODULE_10__get_app_get_app_component__["a" /* GetAppComponent */]
+                __WEBPACK_IMPORTED_MODULE_7__eula_eula_component__["a" /* EulaComponent */],
+                __WEBPACK_IMPORTED_MODULE_9__privacy_privacy_component__["a" /* PrivacyComponent */],
+                __WEBPACK_IMPORTED_MODULE_8__fastpass_fastpass_component__["a" /* FastpassComponent */],
+                __WEBPACK_IMPORTED_MODULE_10__hero_detail_hero_detail_component__["a" /* HeroDetailComponent */],
+                __WEBPACK_IMPORTED_MODULE_11__get_app_get_app_component__["a" /* GetAppComponent */]
             ],
             imports: [
                 __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__["a" /* BrowserModule */],
                 __WEBPACK_IMPORTED_MODULE_2__app_routing_module__["a" /* AppRoutingModule */],
-                __WEBPACK_IMPORTED_MODULE_13__angular_common_http__["b" /* HttpClientModule */]
+                __WEBPACK_IMPORTED_MODULE_14__angular_common_http__["b" /* HttpClientModule */]
             ],
-            providers: [__WEBPACK_IMPORTED_MODULE_11__hero_service__["a" /* HeroService */], __WEBPACK_IMPORTED_MODULE_12__message_service__["a" /* MessageService */]],
+            providers: [__WEBPACK_IMPORTED_MODULE_12__hero_service__["a" /* HeroService */], __WEBPACK_IMPORTED_MODULE_13__message_service__["a" /* MessageService */]],
             bootstrap: [__WEBPACK_IMPORTED_MODULE_3__app_component__["a" /* AppComponent */]]
         })
     ], AppModule);
@@ -319,6 +325,66 @@ var DashboardComponent = (function () {
         __metadata("design:paramtypes", [])
     ], DashboardComponent);
     return DashboardComponent;
+}());
+
+
+
+/***/ }),
+
+/***/ "../../../../../src/app/eula/eula.component.css":
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__("../../../../css-loader/lib/css-base.js")(false);
+// imports
+
+
+// module
+exports.push([module.i, "", ""]);
+
+// exports
+
+
+/*** EXPORTS FROM exports-loader ***/
+module.exports = module.exports.toString();
+
+/***/ }),
+
+/***/ "../../../../../src/app/eula/eula.component.html":
+/***/ (function(module, exports) {
+
+module.exports = "<header>\n    <div class=\"container\">\n        <div class=\"row\">\n            <div class=\"col-sm-7\">\n                <div class=\"header-content\">\n                    <div class=\"header-content-inner\">\n                        <h1>Salva App End User License Agreement (EULA)</h1>\n                        <a href=\"conditions#terms\" class=\"btn btn-outline btn-xl page-scroll\">Read</a>\n                    </div>\n                </div>\n            </div>\n            <div class=\"col-sm-5\">\n                <div class=\"device-container\">\n                    <div class=\"device-mockup iphone6_plus portrait white\">\n                        <div class=\"device main\">\n                            <div class=\"screen\">\n                                <!--   Demo image for screen mockup, you can put an image here, some HTML, an animation, video, or anything else!   -->\n                                <img itemprop=\"screenshot\" itemprop=\"screenshot\" src=\"../assets/img/demo-screen-rayo.png\" class=\"img-responsive\" alt=\"\"/>\n                            </div>\n                            <div class=\"button\">\n                                <!--   You can hook the \"home button\" to some JavaScript events or just remove it   -->\n                            </div>\n                        </div>\n                    </div>\n                </div>\n            </div>\n        </div>\n    </div>\n  </header>\n  <section id=\"terms\" class=\"container\">\n        <div class=\"row\">\n            <div class=\"col-md-12\">\n                <p>Last updated: April 23, 2018</p>\n                <p>\n                    This End User License Agreement (“Agreement”) is between you and Salva and governs use of this app made available through the Apple App Store. By installing the Salva App, you agree to be bound by this Agreement and understand that there is no tolerance for objectionable content. If you do not agree with the terms and conditions of this Agreement, you are not entitled to use the Salva App.\n                </p>\n                <p>\n                    In order to ensure Salva provides the best experience possible for everyone, we strongly enforce a no tolerance policy for objectionable content. If you see inappropriate content, please use the \"Report as offensive\" feature found under each post.\n                </p>\n                <p> \n                    1. Parties This Agreement is between you and Salva only, and not Apple, Inc. (“Apple”). Notwithstanding the foregoing, you acknowledge that Apple and its subsidiaries are third party beneficiaries of this Agreement and Apple has the right to enforce this Agreement against you. Salva, not Apple, is solely responsible for the Salva App and its content.\n                </p>\n                <p>    \n                    2. Privacy Salva may collect and use information about your usage of the Salva App, including certain types of information from and about your device. Salva may use this information, as long as it is in a form that does not personally identify you, to measure the use and performance of the Salva App.\n                </p>\n                <p>    \n                    3. Limited License Salva grants you a limited, non-exclusive, non-transferable, revocable license to use theSalva App for your personal, non-commercial purposes. You may only use theSalva App on Apple devices that you own or control and as permitted by the App Store Terms of Service.\n                </p>\n                <p>    \n                    4. Age Restrictions By using the Salva App, you represent and warrant that (a) you are 17 years of age or older and you agree to be bound by this Agreement; (b) if you are under 17 years of age, you have obtained verifiable consent from a parent or legal guardian; and (c) your use of the Salva App does not violate any applicable law or regulation. Your access to the Salva App may be terminated without warning if Salva believes, in its sole discretion, that you are under the age of 17 years and have not obtained verifiable consent from a parent or legal guardian. If you are a parent or legal guardian and you provide your consent to your child's use of the Salva App, you agree to be bound by this Agreement in respect to your child's use of the Salva App.\n                </p>\n                <p>    \n                    5. Objectionable Content Policy Content may not be submitted to Salva, who will moderate all content and ultimately decide whether or not to post a submission to the extent such content includes, is in conjunction with, or alongside any, Objectionable Content. Objectionable Content includes, but is not limited to: (i) sexually explicit materials; (ii) obscene, defamatory, libelous, slanderous, violent and/or unlawful content or profanity; (iii) content that infringes upon the rights of any third party, including copyright, trademark, privacy, publicity or other personal or proprietary right, or that is deceptive or fraudulent; (iv) content that promotes the use or sale of illegal or regulated substances, tobacco products, ammunition and/or firearms; and (v) gambling, including without limitation, any online casino, sports books, bingo or poker.\n                </p>\n                <p>    \n                    6. Warranty Salva disclaims all warranties about the Salva App to the fullest extent permitted by law. To the extent any warranty exists under law that cannot be disclaimed, Salva, not Apple, shall be solely responsible for such warranty.\n                </p>\n                <p>    \n                    7. Maintenance and Support Salva does provide minimal maintenance or support for it but not to the extent that any maintenance or support is required by applicable law, Salva, not Apple, shall be obligated to furnish any such maintenance or support.\n                </p>\n                <p>    \n                    8. Product Claims Salva, not Apple, is responsible for addressing any claims by you relating to the Salva App or use of it, including, but not limited to: (i) any product liability claim; (ii) any claim that the Salva App fails to conform to any applicable legal or regulatory requirement; and (iii) any claim arising under consumer protection or similar legislation. Nothing in this Agreement shall be deemed an admission that you may have such claims.\n                </p>\n                <p>    \n                    9. Third Party Intellectual Property Claims Salva shall not be obligated to indemnify or defend you with respect to any third party claim arising out or relating to the Salva App. To the extent Salva is required to provide indemnification by applicable law, Salva, not Apple, shall be solely responsible for the investigation, defense, settlement and discharge of any claim that the Salva App or your use of it infringes any third party intellectual property right.\n                    Agree Decline\n                </p>\n            </div>\n        </div>\n  </section>\n  <section class=\"cta\">\n    <div class=\"cta-content\">\n        <div class=\"container\">\n            <h2>No esperes más.<br/>Ahorra.</h2>\n            <a href=\"help#download\" class=\"btn btn-outline btn-xl page-scroll\">Consigue Salva</a>\n        </div>\n    </div>\n    <div class=\"overlay\"></div>\n  </section>\n  "
+
+/***/ }),
+
+/***/ "../../../../../src/app/eula/eula.component.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return EulaComponent; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/esm5/core.js");
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+var EulaComponent = (function () {
+    function EulaComponent() {
+    }
+    EulaComponent.prototype.ngOnInit = function () { };
+    EulaComponent = __decorate([
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["n" /* Component */])({
+            selector: 'app-eula',
+            template: __webpack_require__("../../../../../src/app/eula/eula.component.html"),
+            styles: [__webpack_require__("../../../../../src/app/eula/eula.component.css")]
+        }),
+        __metadata("design:paramtypes", [])
+    ], EulaComponent);
+    return EulaComponent;
 }());
 
 
